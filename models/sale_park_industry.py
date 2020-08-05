974c4581353633ede4e449f2c98c920a8061d783
madukubah <muhalfalah1998@gmail.com>
2020-08-05 11:41:55 +0800

    update

--- models/sale_park_industry.py
+++ models/sale_park_industry.py
@@ -5,8 +5,8 @@ class SaleParkIndustry(models.Model):
     _name = "sale.park.industry"
 
     name = fields.Char(string="Name", size=100 , required=True)
-    currency_80_pc = fields.Float( string="Currency 80%", required=True, default=0 )
-    currency_20_pc = fields.Float( string="Currency 20%", required=True, default=0 )
+    # currency_80_pc = fields.Float( string="Currency 80%", required=True, default=0 )
+    # currency_20_pc = fields.Float( string="Currency 20%", required=True, default=0 )
     factories_ids = fields.One2many(
         'res.partner', 
         'park_industry_id', 
