e0bcfeca81be56ac7ca02250f548b29c443bbaaf
madukubah <muhalfalah1998@gmail.com>
2020-12-18 22:17:12 +0800

    update

--- models/sale_contract.py
+++ models/sale_contract.py
@@ -21,24 +21,24 @@ class SaleContract(models.Model):
     quantity = fields.Float( string="Quantity (WMT)", required=True, default=0, digits=0 )
 
     hma_price = fields.Float( string="HMA Price (IDR)", required=True, default=0, digits=0 )
-    shipping_price = fields.Float( string="Shipping Price", required=True, default=0, digits=0 )
+    shipping_price = fields.Float( string="Shipping Cost", required=True, default=0, digits=0 )
     corrective_factor = fields.Float( string="Corrective Factor (%)", required=True, default=0, digits=0 )
 
     # XX = fields.Float( string="XX", required=True, default=0, digits=0, compute="compute_XX" )
 
-    base_price_select = fields.Selection([
-        ('fix', 'Fixed Amount'),
-        ('code', 'Python Code'),
-    ], string='Base Price Type', index=True, required=True, default='fix', help="The computation method for the rule amount.")
-    base_price = fields.Float( string="Base Price (USD)", required=True, default=0, digits=0 )
-    base_price_python_compute = fields.Text(string='Python Code',
-        default='''
-                    # Available variables:
-                    #----------------------
-                    # qaqc_coa: qaqc.coa.order object
-                    # sale_contract: sale.contract object
-                    # base_price_components: sale.contract.base.price.component object
-                    result = 0''')
+    # base_price_select = fields.Selection([
+    #     ('fix', 'Fixed Amount'),
+    #     ('code', 'Python Code'),
+    # ], string='Base Price Type', index=True, required=True, default='fix', help="The computation method for the rule amount.")
+    # base_price = fields.Float( string="Base Price (USD)", required=True, default=0, digits=0 )
+    # base_price_python_compute = fields.Text(string='Python Code',
+    #     default='''
+    #                 # Available variables:
+    #                 #----------------------
+    #                 # qaqc_coa: qaqc.coa.order object
+    #                 # sale_contract: sale.contract object
+    #                 # base_price_components: sale.contract.base.price.component object
+    #                 result = 0''')
 
     base_price_components = fields.One2many(
         'sale.contract.base.price.component',
@@ -139,23 +139,69 @@ class SaleContract(models.Model):
 
         return self.compute_base_price(baselocaldict)
     
-    #TODO should add some checks on the type of result (should be float)
-    @api.multi
     def compute_base_price(self, localdict):
-        """
-        :param localdict: dictionary containing the environement in which to compute the rule
-        :return: returns a tuple build as the base/amount computed, the quantity and the rate
-        :rtype: (float, float, float)
-        """
         self.ensure_one()
-        if self.base_price_select == 'fix':
-            try:
-                return self.base_price
-            except:
-                raise UserError(_('Wrong quantity defined for contract %s .') % (self.name))
-        else:
-            try:
-                safe_eval(self.base_price_python_compute, localdict, mode='exec', nocopy=True)
-                return float(localdict['result'])
-            except:
-                raise UserError(_('Wrong python code defined for contract %s .') % (self.name))
+        base_price_components = localdict['base_price_components']
+        qaqc_coa = localdict['qaqc_coa']
+        qaqc_coa = localdict['qaqc_coa']
+        sale_contract = self
+        price_component_dict = {
+            'main':[],
+            'add':[],
+            'subtract':[],
+        }
+        for base_price_component in base_price_components.dict :
+            if price_component_dict.get( base_price_component.rule, False):
+                price_component_dict[ base_price_component.rule ] += [ base_price_component ]
+            else:
+                price_component_dict[ base_price_component.rule ] = []
+                price_component_dict[ base_price_component.rule ] += [ base_price_component ]
+
+        hma = sale_contract.hma_price
+        shipping_price = sale_contract.shipping_price
+        corrective_factor = sale_contract.corrective_factor
+
+        hpm_price = 0
+        if price_component_dict[ 'main' ] :
+            main = price_component_dict[ 'main' ][0]
+            main_spec_qaqc = 0
+            for element_spec in qaqc_coa.dict.element_specs :
+                if element_spec.element_id.id == main.element_id.id :
+                    main_spec_qaqc = element_spec.spec
+            diff = main_spec_qaqc - main.spec
+            corrective_factor = corrective_factor + ( diff * 10 ) 
+            hpm_price = hma * ( corrective_factor/100 ) * ( main_spec_qaqc/100 )
+
+            hpm_price_temp = hpm_price
+            for component_add in price_component_dict[ 'add' ] :
+                for element_spec in qaqc_coa.dict.element_specs :
+                    if element_spec.element_id.id == component_add.element_id.id :
+                        hpm_price += hpm_price_temp * (element_spec.spec / 100)
+
+            for component_subtract in price_component_dict[ 'subtract' ] :
+                for element_spec in qaqc_coa.dict.element_specs :
+                    if element_spec.element_id.id == component_subtract.element_id.id :
+                        hpm_price -= hpm_price_temp * (element_spec.spec / 100)
+
+        result = hpm_price + shipping_price
+        return result
+    #TODO should add some checks on the type of result (should be float)
+    # @api.multi
+    # def compute_base_price(self, localdict):
+    #     """
+    #     :param localdict: dictionary containing the environement in which to compute the rule
+    #     :return: returns a tuple build as the base/amount computed, the quantity and the rate
+    #     :rtype: (float, float, float)
+    #     """
+    #     self.ensure_one()
+    #     if self.base_price_select == 'fix':
+    #         try:
+    #             return self.base_price
+    #         except:
+    #             raise UserError(_('Wrong quantity defined for contract %s .') % (self.name))
+    #     else:
+    #         try:
+    #             safe_eval(self.base_price_python_compute, localdict, mode='exec', nocopy=True)
+    #             return float(localdict['result'])
+    #         except:
+    #             raise UserError(_('Wrong python code defined for contract %s .') % (self.name))
